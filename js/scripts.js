28da68e105b9ae98ac7b41eb67e98d905c95a8a1
dpcontreras86-sudo <dpcontreras86@gmail.com>
2026-08-15 18:21:38 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,25 @@
+// Este script hace que el acordeón se abra y se cierre
 
+document.addEventListener('DOMContentLoaded', function() {
+    var acc = document.getElementsByClassName("acordeon-btn");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            // Alterna la clase 'active' para cambiar el color del botón
+            this.classList.toggle("active");
+
+            // Selecciona el panel que está inmediatamente después del botón
+            var panel = this.nextElementSibling;
+            
+            // Si el panel tiene altura, lo cierra. Si no, lo abre.
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+                panel.style.borderBottom = "none";
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+                panel.style.borderBottom = "1px solid #e2e8f0";
+            } 
+        });
+    }
+});
